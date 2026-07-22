997a64ce11f4d262a6e5676a6ca9c5585729267c
jlzlt <juliusrackys@gmail.com>
2026-07-22 11:48:44 +0300

    Block non-members from completing DiscourseConnect SSO login.

--- discourse-woocommerce-functions.php
+++ discourse-woocommerce-functions.php
@@ -9,7 +9,7 @@
  * Author URI: http://thepavilion.io
  */
 
-defined( 'ABSPATH' ) or die( 'No scripts' );
+defined('ABSPATH') or die('No scripts');
 
 use WPDiscourse\Utilities\Utilities as DiscourseUtilities;
 
@@ -26,14 +26,16 @@ $product_group_map[] = (object) array('product_ids' => [], 'group_id' => 41, 'gr
  * WordPress/WooCommerce handles all email verification via Customer Email Verification plugin.
  * All users authenticate via DiscourseConnect SSO - no local Discourse registrations.
  */
-add_filter( 'discourse_email_verification', '__return_false' );
+add_filter('discourse_email_verification', '__return_false');
 
-add_action( 'user_register', 'clear_discourse_email_not_verified', 99, 1 );
-function clear_discourse_email_not_verified( $user_id ) {
-    delete_user_meta( $user_id, 'discourse_email_not_verified' );
+add_action('user_register', 'clear_discourse_email_not_verified', 99, 1);
+function clear_discourse_email_not_verified($user_id)
+{
+	delete_user_meta($user_id, 'discourse_email_not_verified');
 }
 
-function get_discourse_group_ids($plan_id) {
+function get_discourse_group_ids($plan_id)
+{
 	global $member_group_map;
 	$group_ids = array();
 
@@ -49,10 +51,11 @@ function get_discourse_group_ids($plan_id) {
 const ACTIVE_STATUSES = array('wcm-active');
 const INACTIVE_STATUSES = array('wcm-expired', 'wcm-cancelled');
 
-function get_user_group_action($user_id, $group_id) {
+function get_user_group_action($user_id, $group_id)
+{
 	global $member_group_map;
 
-	$plans_for_group = array_filter($member_group_map, function($item) use ($group_id) {
+	$plans_for_group = array_filter($member_group_map, function ($item) use ($group_id) {
 		return $item->group_id == $group_id;
 	});
 
@@ -73,7 +76,8 @@ function get_user_group_action($user_id, $group_id) {
 	return $has_inactive_membership ? 'DELETE' : null;
 }
 
-function update_discourse_group_access($user_id, $action, $group_id) {
+function update_discourse_group_access($user_id, $action, $group_id)
+{
 	$options = DiscourseUtilities::get_options();
 	$logger = wc_get_logger();
 	$sso_secret_key = $options['sso-secret'];
@@ -81,35 +85,36 @@ function update_discourse_group_access($user_id, $action, $group_id) {
 	$sso_client = $options['sso-client-enabled'];
 	$sso_enabled = $sso_secret_key && ($sso_provider || $sso_client);
 
-	if ( empty( $options['url'] ) || empty( $options['api-key'] ) || empty( $options['publish-username'] ) || ! $sso_enabled ) {
-	  return new \WP_Error( 'discourse_configuration_error', 'The WP Discourse plugin has not been properly configured.' );
+	if (empty($options['url']) || empty($options['api-key']) || empty($options['publish-username']) || ! $sso_enabled) {
+		return new \WP_Error('discourse_configuration_error', 'The WP Discourse plugin has not been properly configured.');
 	}
 
-	$logger->info( sprintf('Updating discourse group access %s %s %s', $user_id, $action, $group_id) );
+	$logger->info(sprintf('Updating discourse group access %s %s %s', $user_id, $action, $group_id));
 
-	if ( $sso_provider ) {
-		update_discourse_group_access_provider( $user_id, $action, $group_id );
-	} else if ( $sso_client ) {
-		update_discourse_group_access_client( $user_id, $action, $group_id );
+	if ($sso_provider) {
+		update_discourse_group_access_provider($user_id, $action, $group_id);
+	} else if ($sso_client) {
+		update_discourse_group_access_client($user_id, $action, $group_id);
 	}
 }
 
-function update_discourse_group_access_provider($user_id, $action, $group_id) {
+function update_discourse_group_access_provider($user_id, $action, $group_id)
+{
 	global $member_group_map;
 	global $product_group_map;
 	$logger = wc_get_logger();
 	$group_name = '';
 
-	foreach( $member_group_map as $item ) {
-		if ( $group_id == $item->group_id && !empty($item->group_name) ) {
+	foreach ($member_group_map as $item) {
+		if ($group_id == $item->group_id && !empty($item->group_name)) {
 			$group_name = $item->group_name;
 			break;
 		}
 	}
 
 	if (!$group_name) {
-		foreach( $product_group_map as $item ) {
-			if ( $group_id == $item->group_id && !empty($item->group_name) ) {
+		foreach ($product_group_map as $item) {
+			if ($group_id == $item->group_id && !empty($item->group_name)) {
 				$group_name = $item->group_name;
 				break;
 			}
@@ -121,17 +126,18 @@ function update_discourse_group_access_provider($user_id, $action, $group_id) {
 		return;
 	}
 
-	if ( $action === 'PUT') {
-		DiscourseUtilities::add_user_to_discourse_group( $user_id, $group_name );
-	} else if ( $action === 'DELETE' ) {
-		DiscourseUtilities::remove_user_from_discourse_group( $user_id, $group_name );
+	if ($action === 'PUT') {
+		DiscourseUtilities::add_user_to_discourse_group($user_id, $group_name);
+	} else if ($action === 'DELETE') {
+		DiscourseUtilities::remove_user_from_discourse_group($user_id, $group_name);
 	}
 }
 
-function update_discourse_group_access_client($user_id, $action, $group_id) {
+function update_discourse_group_access_client($user_id, $action, $group_id)
+{
 	$options = DiscourseUtilities::get_options();
 	$logger = wc_get_logger();
-	$external_url = esc_url_raw( $options['url'] . "/groups/". $group_id ."/members" );
+	$external_url = esc_url_raw($options['url'] . "/groups/" . $group_id . "/members");
 	$user_info = get_userdata($user_id);
 	// NOTE: Potential bug in client mode: WP_User does not expose `user_id`.
 	// This should likely read from `discourse_sso_user_id` user meta instead.
@@ -153,27 +159,28 @@ function update_discourse_group_access_client($user_id, $action, $group_id) {
 		'Api-Username' => $options['publish-username'],
 	);
 
-	$logger->info( sprintf('Sending %s request to %s with headers %s and body %s', $action, $external_url, json_encode($headers), json_encode($body)) );
+	$logger->info(sprintf('Sending %s request to %s with headers %s and body %s', $action, $external_url, json_encode($headers), json_encode($body)));
 
 	$response = wp_remote_request(
 		$external_url,
 		array(
-    	 'method' 	=> $action,
-			 'headers' 	=> $headers,
-			 'body'    	=> json_encode($body)
-    )
+			'method' 	=> $action,
+			'headers' 	=> $headers,
+			'body'    	=> json_encode($body)
+		)
 	);
 
-	$logger->info( sprintf( 'Response from Discourse: %s %s' , wp_remote_retrieve_response_code($response), wp_remote_retrieve_response_message($response) ) );
+	$logger->info(sprintf('Response from Discourse: %s %s', wp_remote_retrieve_response_code($response), wp_remote_retrieve_response_message($response)));
 
-	if ( ! DiscourseUtilities::validate( $response ) ) {
-		return new \WP_Error( 'discourse_response_error', 'There has been an error in retrieving the user data from Discourse.' );
+	if (! DiscourseUtilities::validate($response)) {
+		return new \WP_Error('discourse_response_error', 'There has been an error in retrieving the user data from Discourse.');
 	}
 };
 
-function handle_wc_membership_saved($membership_plan, $args) {
+function handle_wc_membership_saved($membership_plan, $args)
+{
 	$logger = wc_get_logger();
-	$logger->info( sprintf('Running handle_wc_membership_saved %s, %s, %s', $args['user_id'], $args['user_membership_id'], $args['is_update'] ) );
+	$logger->info(sprintf('Running handle_wc_membership_saved %s, %s, %s', $args['user_id'], $args['user_membership_id'], $args['is_update']));
 
 	$user_id = $args['user_id'];
 	$membership = wc_memberships_get_user_membership($args['user_membership_id']);
@@ -185,8 +192,8 @@ function handle_wc_membership_saved($membership_plan, $args) {
 
 	$plan_id = $membership->get_plan_id();
 
-	if ( ! $plan_id ) {
-		$logger->info( sprintf('No current membership for %s, %s, %s', $args['user_id'], $args['user_membership_id'], $args['is_update'] ) );
+	if (! $plan_id) {
+		$logger->info(sprintf('No current membership for %s, %s, %s', $args['user_id'], $args['user_membership_id'], $args['is_update']));
 		return;
 	}
 
@@ -196,7 +203,7 @@ function handle_wc_membership_saved($membership_plan, $args) {
 		$action = get_user_group_action($user_id, $group_id);
 
 		if (!$action) {
-			$logger->info( sprintf('No action for user %s group %s - skipping', $user_id, $group_id) );
+			$logger->info(sprintf('No action for user %s group %s - skipping', $user_id, $group_id));
 			continue;
 		}
 
@@ -204,9 +211,10 @@ function handle_wc_membership_saved($membership_plan, $args) {
 	}
 };
 
-function handle_wc_membership_status_change($user_membership, $old_status, $new_status) {
+function handle_wc_membership_status_change($user_membership, $old_status, $new_status)
+{
 	$logger = wc_get_logger();
-	$logger->info( sprintf('Running handle_wc_membership_status_change %s, %s, %s', json_encode($user_membership), $old_status, $new_status ) );
+	$logger->info(sprintf('Running handle_wc_membership_status_change %s, %s, %s', json_encode($user_membership), $old_status, $new_status));
 	return null;
 }
 
@@ -214,7 +222,8 @@ add_action('wc_memberships_user_membership_saved', 'handle_wc_membership_saved',
 
 add_action('wc_memberships_user_membership_status_changed', 'handle_wc_membership_status_change', 10, 3);
 
-function full_wc_membership_sync() {
+function full_wc_membership_sync()
+{
 	global $member_group_map;
 	global $product_group_map;
 
@@ -250,7 +259,9 @@ function full_wc_membership_sync() {
 			return;
 		}
 
-		$plan_ids = array_map(function($item) { return $item->plan_id; }, $member_group_map);
+		$plan_ids = array_map(function ($item) {
+			return $item->plan_id;
+		}, $member_group_map);
 
 		if ($offset === 0) {
 			$count_query = new WP_Query([
@@ -279,7 +290,7 @@ function full_wc_membership_sync() {
 			update_option('discourse_sync_phase', 'orders');
 			return;
 		}
-		
+
 		$logger->info(sprintf('Running full_wc_membership_sync phase memberships - batch offset %s to %s', $offset, $offset + $batch_size));
 
 		$user_group_pairs = array();
@@ -376,7 +387,7 @@ function full_wc_membership_sync() {
 		update_option('discourse_sync_phase', 'memberships');
 		return;
 	}
-	
+
 	$logger->info(sprintf('Running full_wc_membership_sync phase orders - batch offset %s to %s', $offset, $offset + $batch_size));
 
 	$user_group_pairs = array();
@@ -421,7 +432,8 @@ add_action('run_full_wc_membership_sync', 'full_wc_membership_sync');
 
 // Handle single product purchases
 
-function handle_wc_membership_order_status_change( $order_id, $old_status, $new_status ) {
+function handle_wc_membership_order_status_change($order_id, $old_status, $new_status)
+{
 	global $product_group_map;
 	$logger = wc_get_logger();
 
@@ -429,24 +441,57 @@ function handle_wc_membership_order_status_change( $order_id, $old_status, $new_
 		$order = new WC_Order($order_id);
 		$items = $order->get_items();
 
-		$logger->info( sprintf('Order items %s', json_encode($items) ) );
+		$logger->info(sprintf('Order items %s', json_encode($items)));
 
-		foreach ( $items as $item_id => $item ) {
+		foreach ($items as $item_id => $item) {
 			$product_id = $item->get_product_id();
 
-			foreach($product_group_map as $struct) {
-		    if (in_array($product_id, $struct->product_ids)) {
+			foreach ($product_group_map as $struct) {
+				if (in_array($product_id, $struct->product_ids)) {
 					$group_id = $struct->group_id;
 					$user_id = $order->get_user_id();
 					$action = "PUT";
 
-					$logger->info( sprintf('Running update_discourse_group_access %s, %s, %s', $user_id, $action, $group_id ) );
+					$logger->info(sprintf('Running update_discourse_group_access %s, %s, %s', $user_id, $action, $group_id));
 
 					update_discourse_group_access($user_id, $action, $group_id);
-		    }
+				}
 			}
 		}
-  }
+	}
 }
 
 add_action('woocommerce_order_status_changed', 'handle_wc_membership_order_status_change', 10, 3);
+
+/**
+ * Block non-members from completing DiscourseConnect SSO login.
+ */
+function matonizz_gate_discourse_sso($user_id, $user)
+{
+	// Let WordPress admins through so staff never get locked out.
+	if (user_can($user_id, 'manage_options')) {
+		return;
+	}
+
+	if (! function_exists('wc_memberships_get_user_membership')) {
+		return; // Memberships inactive; fail open. Change to redirect for fail-closed.
+	}
+
+	global $member_group_map;
+	$is_active_member = false;
+
+	foreach ($member_group_map as $item) {
+		$membership = wc_memberships_get_user_membership($user_id, $item->plan_id);
+		if ($membership && in_array($membership->status, ACTIVE_STATUSES, true)) {
+			$is_active_member = true;
+			break;
+		}
+	}
+
+	if (! $is_active_member) {
+		wp_safe_redirect(home_url('/membership-required/'));
+		exit;
+	}
+}
+
+add_action('wpdc_sso_provider_before_sso_redirect', 'matonizz_gate_discourse_sso', 10, 2);
